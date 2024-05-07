09d4014d69cbe5ca9cc1204b07bb122e3f0f5249
Malkin Ilya <i-malkin@ya.ru>
2024-05-07 14:32:05 +0400

    убрал все sleep и импорты библиотек для них

--- tests/test_questions_about_important.py
+++ tests/test_questions_about_important.py
@@ -1,7 +1,6 @@
 from pages.main_page import MainPageAction
 from data import AnswerOriginal
 import pytest
-import time
 import allure
 
 
@@ -11,10 +10,9 @@ class TestQuestionsText:
     @pytest.mark.parametrize("questions, hint, text", AnswerOriginal.questions_list)
     def test_questions_drop_down_list(self, browser, questions, hint, text):
         main_page = MainPageAction(browser)
-        main_page.go_to_site() #new
+        main_page.go_to_site()
         main_page.click_close_cookie()
         main_page.scroll_to_list_questions(browser)
-        time.sleep(1)
         main_page.click_drop_down_list(questions)
         answer = main_page.get_text_from_drop_down_list(hint)
         print(answer)
